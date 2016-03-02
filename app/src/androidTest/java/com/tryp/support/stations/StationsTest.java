8c876bcd714f9add49f808337d252cb0d3914df9
Egor Danilenko <edanylenko@cogniance.com>
2016-03-02 16:23:48 +0200

    few ui tests added. brief details added to the map

---
+++ app/src/androidTest/java/com/tryp/support/stations/StationsTest.java
@@ -0,0 +1,114 @@
+package com.tryp.support.stations;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.RemoteException;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
+import android.support.test.uiautomator.Until;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertFalse;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by cliffroot on 02.03.16.
+ */
+
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class StationsTest {
+
+    private static final String BASIC_SAMPLE_PACKAGE = "com.tryp.support";
+    private static final int LAUNCH_TIMEOUT = 5000;
+    private static final String STRING_TO_BE_TYPED = "UiAutomator";
+    private UiDevice mDevice;
+
+    @Before
+    public void startMainActivityFromHomeScreen() {
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+        // Start from the home screen
+        mDevice.pressHome();
+
+        // Wait for launcher
+        final String launcherPackage = mDevice.getLauncherPackageName();
+        assertThat(launcherPackage, notNullValue());
+        mDevice.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)),
+                LAUNCH_TIMEOUT);
+
+        // Launch the app
+        Context context = InstrumentationRegistry.getContext();
+        final Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage(BASIC_SAMPLE_PACKAGE);
+        // Clear out any previous instances
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
+
+        // Wait for the app to appear
+        mDevice.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)),
+                LAUNCH_TIMEOUT);
+    }
+
+    @Test
+    public void showsMap () {
+        assertTrue(mDevice.findObject(new UiSelector()
+                .descriptionContains("Google Map")).exists());
+    }
+
+    @Test
+    public void hasMyLocation () throws UiObjectNotFoundException {
+        UiObject marker = mDevice.findObject(new UiSelector().descriptionContains("me"));
+        marker.click();
+    }
+
+    @Test
+    public void rotatesCorrectly () throws RemoteException {
+        mDevice.setOrientationLeft();
+
+        assertTrue(mDevice.findObject(new UiSelector()
+                .descriptionContains("Google Map")).exists());
+
+        mDevice.unfreezeRotation();
+        mDevice.setOrientationRight();
+
+        assertTrue(mDevice.findObject(new UiSelector()
+                .descriptionContains("Google Map")).exists());
+
+        mDevice.unfreezeRotation();
+        mDevice.setOrientationNatural();
+        assertTrue(mDevice.findObject(new UiSelector()
+                .descriptionContains("Google Map")).exists());
+        mDevice.unfreezeRotation();
+    }
+
+    @Test
+    public void showsBriefDetails () throws  UiObjectNotFoundException {
+        UiObject marker = mDevice.findObject(new UiSelector().descriptionContains("Газовик"));
+        marker.click();
+        assertTrue(mDevice.findObject(new UiSelector().textContains("SEE MORE")).exists());
+        mDevice.findObject(new UiSelector().descriptionContains("Google Map")).clickTopLeft();
+        mDevice.findObject(new UiSelector().textContains("SEE MORE")).waitUntilGone(1000);
+        assertFalse(mDevice.findObject(new UiSelector().textContains("SEE MORE")).exists());
+    }
+
+    @After
+    public void cleanup () throws RemoteException {
+        mDevice.unfreezeRotation();
+    }
+
+
+}
--- app/src/main/java/com/tryp/support/stations/StationsView.java
+++ app/src/main/java/com/tryp/support/stations/StationsView.java
@@ -3,9 +3,12 @@ package com.tryp.support.stations;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.MapView;
@@ -13,17 +16,20 @@ import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Maps;
 import com.google.common.eventbus.Subscribe;
 import com.tryp.support.HostActivity;
 import com.tryp.support.R;
 import com.tryp.support.data.Station;
 import com.tryp.support.utils.LocationReceivedEvent;
+import com.tryp.support.utils.SwipeDismissTouchListener;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ViewById;
 
 import java.util.Collection;
+import java.util.Map;
 
 import java8.util.stream.StreamSupport;
 
@@ -38,6 +44,21 @@ public class StationsView extends Fragment implements StationsContract.View {
     @ViewById(R.id.progress_bar)
     ProgressBar progressBar;
 
+    @ViewById(R.id.station_address)
+    TextView stationAddressView;
+
+    @ViewById(R.id.station_name)
+    TextView stationNameView;
+
+    @ViewById(R.id.station_card)
+    CardView stationCard;
+
+    @ViewById(R.id.see_more_button)
+    Button seeMoreButton;
+
+    Map<Marker, Station> markersToStationsMap = Maps.newHashMap();
+    Marker currentMarker;
+
     StationsContract.UserActionListener actionListener;
 
     public StationsView() { }
@@ -45,6 +66,21 @@ public class StationsView extends Fragment implements StationsContract.View {
     @AfterViews
     void setupMap () {
         mapView.onCreate(null);
+    }
+
+    @AfterViews
+    void setupTouchDismissalListener () {
+        stationCard.setOnTouchListener(new SwipeDismissTouchListener(stationCard, null, new SwipeDismissTouchListener.DismissCallbacks() {
+            @Override
+            public boolean canDismiss(Object token) {
+                return true;
+            }
+
+            @Override
+            public void onDismiss(View view, Object token) {
+                dismissSelection();
+            }
+        }));
     }
 
     @Override
@@ -70,7 +106,9 @@ public class StationsView extends Fragment implements StationsContract.View {
     @Override
     public void onDetach() {
         super.onDetach();
-        mapView.onDestroy();
+        if (mapView != null) {
+            mapView.onDestroy();
+        }
         if (getActivity() != null) {
             ((HostActivity) getActivity()).getEventBus().unregister(this);
         }
@@ -78,8 +116,10 @@ public class StationsView extends Fragment implements StationsContract.View {
 
     @Override
     public void displayProgressBar(boolean active) {
-        progressBar.setVisibility(active ? View.VISIBLE : View.INVISIBLE);
-        mapView.setVisibility(active ? View.INVISIBLE : View.VISIBLE);
+        getActivity().runOnUiThread(() -> {
+            progressBar.setVisibility(active ? View.VISIBLE : View.INVISIBLE);
+            mapView.setVisibility(active ? View.INVISIBLE : View.VISIBLE);
+        });
     }
 
     @Override
@@ -92,8 +132,26 @@ public class StationsView extends Fragment implements StationsContract.View {
                             station -> {
                                 Marker m = map.addMarker(new MarkerOptions().position(station.getPosition()).title(station.getName())
                                         .snippet(station.getAddress()));
+                                markersToStationsMap.put(m, station);
                             }
                     );
+                    map.setOnMarkerClickListener((marker) -> {
+                        if (markersToStationsMap.get(marker) == null) { // this must be our current location marker
+                            return false;
+                        }
+                        currentMarker = marker;
+                        stationCard.setVisibility(View.VISIBLE);
+                        stationNameView.setText(markersToStationsMap.get(marker).getName());
+                        stationAddressView.setText(markersToStationsMap.get(marker).getAddress());
+                        seeMoreButton.setOnClickListener((view) -> {
+                            actionListener.showFullDetails(markersToStationsMap.get(marker));
+                        });
+                        return false;
+                    });
+
+                    map.setOnMapClickListener((point) -> {
+                        dismissSelection();
+                    });
                 });
             });
         }
@@ -120,7 +178,12 @@ public class StationsView extends Fragment implements StationsContract.View {
 
     @Override
     public void dismissSelection() {
+        stationCard.setVisibility(View.GONE);
+        if (currentMarker != null) {
+            currentMarker.hideInfoWindow();
+        }
 
+        currentMarker = null;
     }
 
     @Override
